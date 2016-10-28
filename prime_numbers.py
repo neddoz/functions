72fcbee3757ceea97ba03e634b200506017a39ec
kayeli <kayelidennis@gmail.com>
2016-10-28 16:56:37 +0300

    i fixed a problem when i noticed i was printing zero as a prime number

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,11 +1,14 @@
 import math
 
-for num in range(2,101):
+for num in range(0,101):
     if is_prime(num):
         print(num)
 
 def is_prime(n):
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            return False
+	if n<2:
+		return false
+	elif
+	    for i in range(2, int(math.sqrt(n)) + 1):
+	        if n % i == 0:
+	            return False
     return True
