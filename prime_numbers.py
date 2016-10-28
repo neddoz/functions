85256c0d1c4da6fa9687a2aa1654d2737284a642
kayeli <kayelidennis@gmail.com>
2016-10-28 17:53:48 +0300

    indentation fix

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,13 +1,13 @@
 import math
 
-for num in range(0,101):
+for num in range(2,101):
     if is_prime(num):
         print(num)
 
 def is_prime(n):
 	if n<2:
-		return false
-	elif
+		return False
+	else:
 	    for i in range(2, int(math.sqrt(n)) + 1):
 	        if n % i == 0:
 	            return False
